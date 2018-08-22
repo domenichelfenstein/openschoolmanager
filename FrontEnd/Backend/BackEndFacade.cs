dc4ba6c1b6459c811a8edd6d3e88ded2803c87f0
Domenic Helfenstein <dom.helfenstein@gmail.com>
2018-08-22 22:11:59 +0200

    nicht mehr ssl

--- FrontEnd/Backend/BackEndFacade.cs
+++ FrontEnd/Backend/BackEndFacade.cs
@@ -18,8 +18,7 @@ namespace FrontEnd.Backend
             var settings = MongoClientSettings.FromUrl(
                 new MongoUrl(connectionString)
             );
-            settings.SslSettings =
-                new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
+            settings.UseSsl = false;
             var mongoClient = new MongoClient(settings);
             this.db = mongoClient.GetDatabase("schooldb");
         }
