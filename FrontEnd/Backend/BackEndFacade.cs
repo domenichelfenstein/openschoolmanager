033804f3b10721b755aefa11f1940143540a60a5
Domenic Helfenstein <domenic.helfenstein@edulu.ch>
2018-08-22 20:08:04 +0200

    MongoDB

--- FrontEnd/Backend/BackEndFacade.cs
+++ FrontEnd/Backend/BackEndFacade.cs
@@ -1,4 +1,8 @@
-﻿namespace FrontEnd.Backend
+﻿using System.Linq;
+using System.Security.Authentication;
+using MongoDB.Driver;
+
+namespace FrontEnd.Backend
 {
     using System;
     using System.Collections.Generic;
@@ -6,34 +10,101 @@
 
     public class BackEndFacade : IBackEndFacade
     {
-        public Task CreateCourse(string name, Guid id)
+        private readonly IMongoDatabase db;
+
+        public BackEndFacade()
         {
-            return null;
+            var connectionString = Environment.GetEnvironmentVariable("dbconnection");
+            var settings = MongoClientSettings.FromUrl(
+                new MongoUrl(connectionString)
+            );
+            settings.SslSettings =
+                new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
+            var mongoClient = new MongoClient(settings);
+            this.db = mongoClient.GetDatabase("schooldb");
         }
 
-        public Task<IReadOnlyCollection<Course>> GetAllCourses()
+        #region Courses
+
+        private IMongoCollection<dynamic> GetCoursesHandle()
         {
-            return null;
+            return this.db.GetCollection<dynamic>("courses");
         }
 
-        public Task<Course> GetCourse(Guid id)
+        public async Task CreateCourse(string name, Guid id)
         {
-            return null;
+            await this.GetCoursesHandle().InsertOneAsync(
+                FromCourse(
+                    new Course(
+                        id,
+                        name)));
         }
 
-        public Task CreateStudent(Student student)
+        public async Task<IReadOnlyCollection<Course>> GetAllCourses()
         {
-            return null;
+            var all = await this.GetCoursesHandle()
+                .FindAsync(FilterDefinition<dynamic>.Empty);
+            return all
+                .ToEnumerable()
+                .Select(ToCourse)
+                .ToArray();
         }
 
-        public Task<IReadOnlyCollection<Student>> GetStudents(Guid courseId)
+        public async Task<Course> GetCourse(Guid id)
         {
-            return null;
+            var all = await this.GetAllCourses();
+            return all
+                .SingleOrDefault(x => x.Id == id);
         }
 
-        public Task<Student> GetNextStudentToLearn(Guid courseId)
+        public static Course ToCourse(dynamic x) => new Course(x._id, x.name);
+        public static dynamic FromCourse(Course x) => new { id = x.Id, name = x.Name };
+
+        #endregion
+
+        #region Students
+        private IMongoCollection<dynamic> GetStudentsHandle()
         {
-            return null;
+            return this.db.GetCollection<dynamic>("students");
         }
+
+        public async Task CreateStudent(Student student)
+        {
+            await this.GetStudentsHandle().InsertOneAsync(
+                FromStudent(
+                    student));
+        }
+
+        public async Task<IReadOnlyCollection<Student>> GetStudents(Guid courseId)
+        {
+            var all = await this.GetStudentsHandle()
+                .FindAsync(FilterDefinition<dynamic>.Empty);
+            return all
+                .ToEnumerable()
+                .Select(ToStudent)
+                .Where(x => x.CourseId == courseId)
+                .ToArray();
+        }
+
+        public async Task<Student> GetNextStudentToLearn(Guid courseId)
+        {
+            var allStundentsOfCourse = await this.GetStudents(courseId);
+            var random = new Random();
+            if (!allStundentsOfCourse.Any())
+            {
+                throw new Exception("no students");
+            }
+
+            var index = random.Next(0, allStundentsOfCourse.Count - 1);
+
+            return allStundentsOfCourse
+                .ToArray()[index];
+        }
+        public static dynamic FromStudent(Student x)
+            => new { courseid = x.CourseId, image = x.ImageInBase64, firstname = x.Firstname, lastname = x.Lastname };
+        public static Student ToStudent(dynamic x)
+            => new Student(x.courseid, x.firstname, x.lastname, x.image);
+
+        #endregion
     }
 }
